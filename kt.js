acd1535228c5dfc0aa2eeb21156952ddb44e631f
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-10-03 12:08:53 +1000

    Add Neighbour nodes in knightMove function

--- kt.js
+++ kt.js
@@ -1,116 +1,131 @@
 
+const masterArray = []
 
 
-const knightMove = (coord1, coord2) => {
-    if(coord1<0||coord1>7||coord1===undefined || coord2 === undefined || coord2>7 || coord2===undefined){
+class Node{
+  constructor(row, col, distanceFromStart){
+    this.row = row;
+    this.col = col;
+    this.distanceFromStart = distanceFromStart;
+  }
+
+}
+let n=0
+const visited = new Set([])
+
+//find neighbouring nodes - this needs purging of unnecessary elements
+const knightMove = ([row, col], [lastRow, lastCol]) => {
+
+  console.log(`knightMove`)
+  
+    if(row<0||row>7||row===undefined || col === undefined || col>7 || col===undefined){
         throw new Error("2 coordinates between 0 & 7 required")
     }
-const destinations = []
+// const destinations = []
 const coordModifiers = [[-1, 1], [-2, 2]]
-const adjacencyList =[]
-const adjList = {}
-const coords = [coord1,coord2]
+// const adjacencyList =[]
+// const adjList = []
 
 
-
-const myObject = { existing: "value" };
-const propertiesToAdd = { newKey1: "value1", newKey2: "value2" };
-
-const updatedObject = Object.assign({}, myObject, propertiesToAdd);
-console.log(updatedObject); // Output: { existing: 'value', newKey1: 'value1', newKey2: 'value2' }
-
-// Or directly modify the original object:
-Object.assign(myObject, propertiesToAdd);
-console.log(myObject); // Output: { existing: 'value', newKey1: 'value1', newKey2: 'value2' }
+const coordLinks = []
+const startPosition = new Node(row, col, n)
 
 
     coordModifiers[0].forEach(a => {
        coordModifiers[1].forEach(b => {
-       
-        if((coord1+a)>=0 && (coord1+a<=7) && (coord2+b)>=0 && (coord2+b)<=7){
-           destinations.push([coord1+a, coord2+b])
-           Object.assign(adjList, {coords: [coord1+a, coord2+b]})
-        //    if(!adjacencyList[coord1+a]){
-            for(let i= adjacencyList.length-1; i<=coord1+a; i++){
-                adjacencyList.push([])
+      
+        // console.log(`current elements:`)
+        //     console.log(`row: ${row}, col: ${col}`)
+        //     console.log(`lastRow: ${lastRow}, lastCol: ${lastCol} `)
+        //     console.log(`a: ${a}, b: ${b}`)
+        if((row+a)>=0 && (row+a<=7) && (col+b)>=0 && (col+b)<=7){
+          const newRow = row+a;
+        const newCol = col+b
+          //  destinations.push([newRow, newCol])
+          //  coordLinks.push([newRow, newCol])
+           
+          
+      
+          //   for(let i= adjacencyList.length-1; i<=row+a; i++){
+          //       adjacencyList.push([])
 
-            }
+          //   }
+          
+            const neighbour = new Node(newRow, newCol)
            
-           adjacencyList[coord1+a].push(coord2+b)
+            const neighbourCoordinates = (JSON.stringify([neighbour.row,neighbour.col]))
+            
+           if(!visited.has(neighbourCoordinates)){
+            visited.add(neighbourCoordinates)
+           
+             console.log(`Neighbour of: [${row},${col}] -- [${newRow},${newCol}]`)
+              
+            knightMove([newRow, newCol], [lastRow, lastCol])
+           
+           } 
+
+           if(neighbour.row === lastRow && neighbour.col === lastCol){
+            console.log(`Found: ${[lastRow, lastCol]}`)
+           }           
+          //  adjacencyList[newRow].push(newCol)
+          //  knightMove([newRow, newCol], [lastRow, lastCol])
         }
-         if((coord2+a)>=0 && (coord2+a<=7) && (coord1+b)>=0 && (coord1+b)<=7){
-           destinations.push([coord1+b, coord2+a])
-            adjList.coords = [coord1+b, coord2+a]
-        
-            for(let i= adjacencyList.length-1; i<=coord1+b; i++){
-                adjacencyList.push([])
-               
-                              }
+         if((col+a)>=0 && (col+a<=7) && (row+b)>=0 && (row+b)<=7){
+           const newRow = row+b;
+        const newCol = col+a
+          //  destinations.push([newRow, newCol])
+          //  coordLinks.push([newRow, newCol])
            
-           adjacencyList[coord1+b].push(coord2+a)  }
+        
+            // for(let i= adjacencyList.length-1; i<=row+b; i++){
+            //     adjacencyList.push([])
+               
+            //                   }
+          // console.log(`current elements (b,a):`)
+          //   console.log(`row: ${row}, col: ${col}`)
+          //   console.log(`lastRow: ${lastRow}, lastCol: ${lastCol} `)
+          //   console.log(`a: ${a}, b: ${b}`)
+           const neighbour = new Node(newRow, newCol, n+1)
+           
+            const neighbourCoordinates = (JSON.stringify([neighbour.row,neighbour.col]))
+            
+           if(!visited.has(neighbourCoordinates)){
+            visited.add(neighbourCoordinates)
+            
+            knightMove([newRow, newCol], [lastRow, lastCol])
+          
+           } 
+            if(newRow === lastRow && newCol === lastCol){
+          console.log(`Found: ${[lastRow, lastCol]}: ${n}`)
+           }     
+          //  adjacencyList[newRow].push(newCol)  
+         
+          }
+           
         })
+       
     })
 
-    // const square = new Link(coord1, coord2)
+   masterArray.push([[row, col], coordLinks])
    
+    // if(!visited.has([lastRow,lastCol])){
 
-return {destinations, adjacencyList, adjList}
+    // }
+
+// return {destinations, adjacencyList, adjList}
 
 
 
 }
 
+console.log(knightMove([3,3], [5,3]))
+
+//  const visitNeighbours = () => {
+
+//  }
+
+// console.log(visited)
+// console.log(knightMove.destinations)
 
 
-console.log(knightMove(3,3))
 
-
-
-const levelOrderForEach =(callback, node) =>{
-    const queueArray = [node]
-
-    if (callback === undefined) {
-      throw new Error("Argument must contain a function")
-
-    } else {
-
-      while (queueArray.length >= 1) {
-
-        callback(queueArray[0])
-
-        if (queueArray[0].left !== null && queueArray[0].right !== null) {
-          queueArray.push(queueArray[0].left, queueArray[0].right);
-
-        } else if (queueArray[0].left !== null && queueArray[0].right === null) {
-            queueArray.push(queueArray[0].left)
-
-          } else if (queueArray[0].right !== null && queueArray[0].left === null) {
-              queueArray.push(queueArray[0].right)
-            }
-
-        queueArray.shift(queueArray[0])
-
-      }
-    }
-  }
-
-  class Graph {
-    constructor(square){
-        this.square = square
-        this.linkArray = knightMove(square[0], square[1]).adjacencyList
-    }
-  }
-
-  class Link {
-    constructor(coords) {
-        this.coords = coords;
-        this.link1= null;
-        this.link3 = null;
-        this.link4 = null;
-        this.link5=null;
-        this.link6=null;
-        this.link7=null;
-        this.link8=null
-
-    }
-  }
