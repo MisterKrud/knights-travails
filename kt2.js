51b69a56ab4827acd654b98b1f4ee4ab8a0eff21
MisterKrud <162855452+MisterKrud@users.noreply.github.com>
2025-10-05 13:03:30 +1100

    Fix bug in createData() and getNeighbours()

--- kt2.js
+++ kt2.js
@@ -1,13 +1,6 @@
-class Node {
-  constructor(row, col) {
-    this.row = row;
-    this.col = col;
-    this.neighbours = new Set([]);
-    this.coordinates = [row, col];
-  }
-}
-const visited = new Set([])
-const adjacencyList = new Map()
+
+
+
 
 class Board{
   constructor([row, col], [endRow, endCol]){
@@ -20,17 +13,25 @@ class Board{
 }
 
 
+class Node {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+    this.neighbours = new Set([]);
+    this.coordinates = [row, col];
+  }
+}
+
+const visited = new Set([]);
+const adjacencyList = new Map();
+
 //Get neighbours of square
-const getNeighbours = ([row, col], [endRow, endCol]) => {
-  
+const getNeighbours = ([row, col]) => {
   //Make a node of initial square
   let start = new Node(row, col);
-  //add it to 'visited' set
- 
 
   //Don't allow numbers outside of 0-7 in coordinates
-  const neighbourSet = new Set([]);
-  const neighbourCoords = []
+
   if (
     row < 0 ||
     row > 7 ||
@@ -41,82 +42,81 @@ const getNeighbours = ([row, col], [endRow, endCol]) => {
   ) {
     console.log(`Coordinates must be between 0 & 7`);
   } else {
-
-//Get neighbours coordinates by adding +/-1 & +/-2 to coordinates
-  const coordModifiers = [[-1, 2], [1, 2], [-1, -2], [1, -2], [-2, 1], [2, 1], [-2, -1], [2, -1]];
-   const startCoordinates = (JSON.stringify(start.row)+','+JSON.stringify(start.col))
-    visited.add(startCoordinates);
-      for (let coord of coordModifiers) {
-      const newRow = row+coord[0];
-      const newCol = col+coord[1];
-      if(newRow >=0 && newRow <=7 && newCol >=0 && newCol <=7){
-      if(newRow === endRow && newCol === endCol){
-        console.log(newRow, newCol)
-      }
-      
-      const neighbour = new Node(newRow, newCol)
-      start.neighbours.add(neighbour)
-      
-    
-    
-       const neighbourCoordinates = JSON.stringify(neighbour.row)+','+JSON.stringify(neighbour.col);
-       console.log(`START AND NEIGHBOUR COORDINATES`)
-       console.log(neighbourCoordinates)
-       console.log(startCoordinates)
-          if (!visited.has(neighbourCoordinates)) {
-       neighbourSet.add([neighbourCoordinates]);
-          neighbourCoords.push([neighbour.row, neighbour.col])
-          visited.add(neighbourCoordinates);
-         }
-         
+    //Get neighbours coordinates by adding +/-1 & +/-2 to coordinates
+    const coordModifiers = [
+      [-1, 2],
+      [1, 2],
+      [-1, -2],
+      [1, -2],
+      [-2, 1],
+      [2, 1],
+      [-2, -1],
+      [2, -1],
+    ];
+ 
+    for (let coord of coordModifiers) {
+      const newRow = row + coord[0];
+      const newCol = col + coord[1];
+      if (newRow >= 0 && newRow <= 7 && newCol >= 0 && newCol <= 7) {
+        const neighbourCoordinates = `${newRow}, ${newCol}`;
+        if (!visited.has(neighbourCoordinates)) {
+          const neighbour = new Node(newRow, newCol);
+          start.neighbours.add(neighbour);
+        }
       }
     }
-   adjacencyList.set(startCoordinates, neighbourCoords);
   }
-  console.log(`Neighbour set`)
-  console.log(neighbourSet)
- 
+  return start;
+};
+
+const createData = ([row, col], [endRow, endCol]) => {
+  visited.add(`${row}, ${col}`)
+  const queueArray = [getNeighbours([row, col])];
+
+  while (queueArray.length >= 1) {
   
-
-
-
- return start;
+      
+      const currentCell = queueArray.shift();;
+      const neighbourList = currentCell.neighbours;
+      const currentCoordinates = `${currentCell.row}, ${currentCell.col}`;
+      const neighbourCoords = [];
+      
+     
+      adjacencyList.set(currentCoordinates, neighbourCoords);
+      neighbourList.forEach((neighbour) => {
+       
+        const neighbourCoordinates = `${neighbour.row}, ${neighbour.col}`;
+        if (!visited.has(neighbourCoordinates)) {
+           const newNeighbour = getNeighbours([neighbour.row, neighbour.col]);
+          neighbourCoords.push([neighbour.row, neighbour.col]);
+          visited.add(neighbourCoordinates);
+           queueArray.push(newNeighbour);
+        }
+      });
+ 
+      
+  
+    if (currentCell.row === endRow && currentCell.col === endCol) {
+      return adjacencyList;
+    }
+  
+  }
+ 
 };
 
 
-const createData = ([row, col], [endRow, endCol]) => {
-   //Make a node of initial square
-  let start = getNeighbours([row, col], [endRow, endCol]);
-  //add it to 'visited' set
-  const queueArray =[start];
-  while(queueArray.length>=1){
-    if(queueArray[0].row = endRow && queueArray[0].col === endCol){ 
-      return
-    }
+console.log(visited)
+  console.log(getNeighbours([3,3]))
 
-    if(queueArray[0].neighbours !== null){
-      const neighbourList = queueArray[0].neighbours  
-     neighbourList.forEach(neighbour => {
-        const newNeighbour = getNeighbours([neighbour.row, neighbour.col], [endRow, endCol])
-        queueArray.push(newNeighbour)
-      })
-    } 
-    queueArray.shift(queueArray[0])  
-  }
- return start
-}
+createData([3,3],[5,7])
 
-  
 
-console.log(createData([3,3],[2,5]))
-console.log(`<<<<<<<<<<<<<<<<<<<<<<<ALL MOVES>>>>>>>>>>>>>>>>>>>>>>>`)
 
-console.log(`-----------------------`)
 console.log(adjacencyList)
 
-console.log(visited)
 console.log(`%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%`)
 
+console.log(visited)
 
 
 
